9fd4efe7ca6c8167418d8d32be880015ab748adb
AdityaDees <adityads@ymail.com>
2020-03-30 23:40:21 +0700

    new data

--- 18 - Binary Search.cpp
+++ 18 - Binary Search.cpp
@@ -3,52 +3,57 @@
 
 using namespace std;
 int main () {
-	int n=5, A[5],i, kiri, kanan, tengah, temp, key;
+	int n=5, A[5],i, kiri, kanan, tengah, temp, key,x;
 	bool ketemu = false;
 
 	cout<<"Masukan 5 angka kedalam array "<<endl;
 	for (i=0; i<5; i++) {
-        cin>>A[i];
+		cin>>A[i];
 	}
 	
 	for (int i=0; i<n; i++) {
-  		for(int j=0; j< n-i-1; j++) {
-   			if(A [j] > A [j+1]) {
-    			temp=A[j];
-    			A[j]=A[j+1];
-    			A[j+1]=temp;
-		   }
-  		}
- 	}
- 	
+		for(int j=0; j< n-i-1; j++) {
+			if(A [j] > A [j+1]) {
+				temp=A[j];
+				A[j]=A[j+1];
+				A[j+1]=temp;
+			}
+		}
+	}
+	
 	cout<<"Data yang telah diurutkan adalah : ";
- 	for(int i=0; i<n; i++) {
-  		cout<<A[i]<<" ";
+	for(int i=0; i<n; i++) {
+		cout<<A[i]<<" ";
 	}
- 
- 	cout<<"\n Masukan angka yang dicari : ";
- 	cin>>key;
+	
+	cout<<"\n Masukan angka yang dicari : ";
+	cin>>key;
 
- 	kiri=0;
- 	kanan=n-1;
+	kiri=0;
+	kanan=n-1;
 
- 	while(kiri<=kanan) {
-  		tengah=(kiri + kanan)/2;
-	  	if(key == A[tengah]) {
-	   		ketemu=true;
-	   		break;
-	    } else if (key < A[tengah]) {
-	   		kanan = tengah -1;
+	while(kiri<=kanan) {
+		tengah=(kiri + kanan)/2;
+		if(key == A[tengah]) {
+			ketemu=true;
+			break;
+		} else if (key < A[tengah]) {
+			kanan = tengah -1;
 		} else {
-	   		kiri = tengah +1;
-	  	}
+			kiri = tengah +1;
+		}
 	}
- 	
+	
 	if (ketemu == true) {
-  		cout<<"Angka ditemukan!";
+		cout<<"\nMasukkan data yang baru: ";cin>>A[tengah];
+		cout<<"\nData yang baru: ";
+		for(i=0;i<n;i++)
+		{
+			cout<<"\nData ke-"<<i<<" :"<<A[i];
+		}
 	} else {
-  		cout<<"Angka tidak ditemukan";
+		cout<<"Angka tidak ditemukan";
 	}
-  	return 0;
+	return 0;
 	getch();
- }
+}
--- 24 - Mean Bubble Sort.cpp
+++ 24 - Mean Bubble Sort.cpp
@@ -1,60 +1,69 @@
-#include <iostream>
+#include<iostream>
 #include <conio.h>
+
 using namespace std;
-void swap(int *xp, int *yp)  
-{  
-    int temp = *xp;  
-    *xp = *yp;  
-    *yp = temp;  
-}  
-void bubbleSort(int arr[])  
-{  
-    int i, j;  
-    int n=3;
-    for (i = 0; i < n-1; i++)      
-      
-    // Last i elements are already in place  
-    for (j = 0; j < n-i-1; j++)  
-        if (arr[j] > arr[j+1])  
-            swap(&arr[j], &arr[j+1]);  
-}  
-  
-  
-void hitungRata(int alpro[10],int basdat[10],int pancasila[10]){
-    int i;
-    int rata[3],totalAlpro=0,totalBasdat=0,totalPancasila=0;
+
+void swap(float *xp, float *yp) 
+{ 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
+} 
+
+void bubbleSort(float arr[]) 
+{ 
+	int i, j; 
+	int n=3;
+	for (i = 0; i < n-1; i++)	 
 	
+	for (j = 0; j < n-i-1; j++) {
+		if (arr[j] > arr[j+1]) {
+			swap(&arr[j], &arr[j+1]); 
+		}
+	}
+} 
+
+void printArray(float arr[]) 
+{ 
+	int i; 
+	for (i = 0; i < 3; i++) {
+		cout << arr[i] << " "; 
+	}
+	cout << endl; 
+} 
+
+float *rataNilai(int mataKuliah[3][10]){
+	int i,j;
+	static float arr[3];
 	
-	for(i=0;i<10;i++) {
-		totalAlpro += alpro[i];
-		totalBasdat += basdat[i];
-		totalPancasila += pancasila[i];
+	for(i=0; i<3; i++){
+		for(j=0; j<10; j++){
+			arr[i] += mataKuliah[i][j];
+		}
 	}
 	
-	rata[0] = totalAlpro/10;
-  	rata[1] = totalBasdat/10;
-  	rata[2] = totalPancasila/10;
-  	
-	for(i=0;i<3;i++){
-  	//	cout << rata[i] << endl;
+	for(int i=0;i<3;i++){
+		arr[i] = arr[i]/10;
 	}
 	
-	bubbleSort(rata);
+	return arr;
+}
+
+int main() 
+{ 
+	int i,j;
+ 	float *p;
+ 	int mataKuliah[3][10] = {
+		 {20,100,89,74,75,25,13,86,44,78},
+		 {74,23,78,41,56,83,90,67,34,88},
+		 {82,35,86,95,43,79,45,78,98,54}
+	 };
 	
-}
-void printArray(int arr[], int size)  
-{  
-    int i;  
-    for (i = 0; i < size; i++)  
-        cout << arr[i] << " ";  
-    cout << endl;  
-}
-int main(){
-  	int i;
-    int alpro[10] = {20,100,89,74,75,25,13,86,44,78};  
-    int basdat[10] = {74,23,78,41,56,83,90,67,34,88};  
-    int pancasila[10] = {82,35,86,95,43,79,45,78,98,54}; 
-  
-   	hitungRata(alpro,basdat,pancasila);
-}
-   
+	p = rataNilai(mataKuliah); 
+	
+	bubbleSort(p); 
+	cout<<"Sorted array: \n"; 
+	printArray(p); 
+	getch();
+} 
+
